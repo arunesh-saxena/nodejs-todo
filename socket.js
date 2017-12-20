3b5e4e3f651f114b59d172379fcd4e8a8aa707ee
asax24 <asaxena24@sapient.com>
2017-12-20 10:11:41 +0530

    socket implementation

--- socket.js
+++ socket.js
@@ -1,11 +1,23 @@
-const io = require('socket.io')();
-
+const socket = require('socket.io');
+const io = socket();
+let clientIds = [];
 io.on('connection', (client) => {
   console.log('a user connected');
-
+  clientIds.push(client.id);
   client.on('subscribeToMsg', (data) => {
-    console.log('client is subscribing  ', data);
-    client.emit('msgFromServer', {msg: `web socket : ${data.msg}`});
+    console.log('client is subscribing  ', data,client.id);
+
+    /* send only to just connected clinet */
+    // client.emit('msgFromServer', {msg: `web socket : ${data.msg}`});
+    
+    /* send to all connected client */
+    io.sockets.emit('msgFromServer', {msg: `web socket : ${data.msg}`});
+
+    /* send to specific client */
+    // client.to(clientIds[0]).emit('msgFromServer', {msg: `web socket : ${data.msg}`});
+
+    /* send to all connected client excepted send */
+    // client.broadcast.emit('msgFromServer', {msg: `web socket : ${data.msg}`});
   });
 
   client.on('disconnect', function (reason) {
