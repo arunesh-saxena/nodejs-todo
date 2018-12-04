176eb3666eb26df1161a4c387be11a026ab37eeb
Arunesh Saxena <asaxena24@sapient.com>
2018-12-04 13:05:26 +0530

    todos table schema

--- src/models/Todo.js
+++ src/models/Todo.js
@@ -3,10 +3,11 @@ var mongoose = require('mongoose');
 
 // Create a schema - this is like a blueprint
 var todoSchema = new mongoose.Schema({
-    id: Number,
-    isCompleted: Boolean,
-    isDeleted: Boolean,
-    name: String
+    id: {type: Number, default: 1 },
+    isCompleted: { type: Boolean, default: false },
+    isDeleted: { type: Boolean, default: false },
+    name: String,
+    createdAt: { type: Date, default: Date.now },
 });
 
 var Todo = mongoose.model('todos', todoSchema);
