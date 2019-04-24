03092e2bb38dd445d0b70c31143e659d82d2f0d0
Arunesh Saxena <asaxena24@sapient.com>
2019-04-24 13:49:03 +0530

    indending

--- src/models/restro/Menu.js
+++ src/models/restro/Menu.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 
 let menuSchema = new mongoose.Schema({
-    id:{type: Number, default:1},
-    itemCode:{type: String, default:null},
-    itemName:{type: String, require: true},
-    description:{type: String},
-    imageURL:{type: String},
-    price: {type: Number, required: true},
-    unit: {type: String, required: true},
-    currency: {type: String, required: true},
+    id: { type: Number, default: 1 },
+    itemCode: { type: String, default: null },
+    itemName: { type: String, require: true },
+    description: { type: String },
+    imageURL: { type: String },
+    price: { type: Number, required: true },
+    unit: { type: String, required: true },
+    currency: { type: String, required: true },
     isDeleted: { type: Boolean, default: false },
     createdAt: { type: Date },
     updatedAt: { type: Date, default: Date.now }
@@ -16,15 +16,15 @@ let menuSchema = new mongoose.Schema({
 
 var Menu = mongoose.model('menu', menuSchema);
 
-menuSchema.pre('save', function(next) {
+menuSchema.pre('save', function (next) {
     var doc = this;
-    Menu.find({}).select('id').sort({id: -1}).limit(1).exec(function(err, data){
-        if(data.length){
+    Menu.find({}).select('id').sort({ id: -1 }).limit(1).exec(function (err, data) {
+        if (data.length) {
             doc.id = ++data[0].id;
-            doc.itemCode = `${doc.itemName.substr(0,3)}${doc.id}`;
-        }else{
-            doc.itemCode = `${doc.itemName.substr(0,3)}${doc.id}`;
-        }        
+            doc.itemCode = `${doc.itemName.substr(0, 3)}${doc.id}`;
+        } else {
+            doc.itemCode = `${doc.itemName.substr(0, 3)}${doc.id}`;
+        }
         doc.createdAt = Date.now();
         next();
     });
